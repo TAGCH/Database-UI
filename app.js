c31733699062fc1bc160475d8028378d40c6e8d9
TAGCH <pisit.au@ku.th>
2023-04-07 15:46:52 +0700

    First Commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var express = require( "express" );
+var app = express();
+
+app.set ( "view engine", "ejs" );
+
+const person = [{
+    name: 'Kevin',
+    age: 17,
+    canCook: true,
+}, {
+    name: 'Gavin',
+    age: 19,
+    canCook: true,
+}, {
+    name: 'Sean',
+    age: 10,
+    canCook: false,
+}];
+
+app.get( "/", function (req, res)
+    {
+        res.render ("homepage", {
+            person,
+        });
+    }
+)
+
+app.get ( "/category", function (req, res)
+    {
+        res.render ( "Category" );
+    }
+)
+
+app.get ( "/sales", function (req, res)
+    {
+        res.render ( "Sales" );
+    }
+)
+
+app.get ( "/franchise", function (req, res)
+    {
+        res.render ( "Franchise" );
+    }
+)
+
+
+app.listen (8081, "127.0.0.2",()=> {
+    console.log("URL: http://127.0.0.2:8081");
+} );
