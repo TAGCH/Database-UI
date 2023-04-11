0a6207bcdb25e8c013a3235d90f4fc8acff55fb6
TAGCH <pisit.au@ku.th>
2023-04-12 01:27:10 +0700

    Update Data

--- app.js
+++ app.js
@@ -1,49 +1,103 @@
 var express = require( "express" );
 const pool = require("./database");
+const bodyParser = require ("body-parser");
 
 var app = express();
 
 app.set ( "view engine", "ejs" );
+app.use(bodyParser.urlencoded ({extended: false}));
 
-const person = [{
-    name: 'Kevin',
-    age: 17,
-    canCook: true,
-}, {
-    name: 'Gavin',  
-    age: 19,
-    canCook: true,
-}, {
-    name: 'Sean',
-    age: 10,
-    canCook: false,
-}];
+
+app.post ("/Search", async (req, res) => {
+    try {
+        const request = pool.request();
+        const result = await request.query('SELECT * from Restaurant');
+        result.recordset.forEach(function(row){
+            if (row.RestaurantName == req.body.search){
+                var Name = row.RestaurantName
+                res.render ("Search", {
+                    result,Name,
+                })
+                res.send(row.RestaurantName);
+            }
+        })
+      } catch (err) {
+        console.log(err);
+        res.status(500).send('Error retrieving users from database');
+      }
+})
 
 
 app.get('/users', async (req, res) => {
     try {
-      const request = pool.request();
-      const result = await request.query('SELECT * FROM Restaurant');
-      res.send(result.recordset);
-    } catch (err) {
-      console.log(err);
-      res.status(500).send('Error retrieving users from database');
-    }
+        const request = pool.request();
+        const result = await request.query('SELECT Rank from Restaurant');
+        res.send(result.recordset);
+      } catch (err) {
+        console.log(err);
+        res.status(500).send('Error retrieving users from database');
+      }
   });
   
 
 
-app.get( "/", function (req, res)
+app.get( "/", async (req, res) =>
     {
-        res.render ("homepage", {
-            person,
-        });
+        try {
+            const request = pool.request();
+            const result = await request.query('SELECT Rank,RestaurantName from Restaurant ORDER by Rank');
+            res.render ("homepage", {
+                result,
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('Error retrieving users from database');
+        }
     }
 )
 
-app.get ( "/Category.ejs", function (req, res)
+app.get ( "/Category.ejs", async (req, res) =>
     {
-        res.render ( "Category" );
+        try {
+            const request = pool.request();
+            const result = await request.query('SELECT Top 50 Rank,RestaurantName,Category.RestaurantType FROM Restaurant,Category Where Restaurant.RestaurantID = Category.RestaurantID');
+            res.render ("Category", {
+                result,
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('Error retrieving users from database');
+        }
+    }
+)
+
+app.get ( "/R_Cat.ejs", async (req, res) =>
+    {
+        try {
+            const request = pool.request();
+            const result = await request.query('SELECT Top 50 Rank,RestaurantName,Category.RestaurantType FROM Restaurant,Category Where Restaurant.RestaurantID = Category.RestaurantID');
+            res.render ("R_Cat", {
+                result,
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('Error retrieving users from database');
+        }
+    }
+)
+
+app.get ( "/C_Cat.ejs", async (req, res) =>
+    {
+        try {
+            const request = pool.request();
+            const result = await request.query('SELECT Top 50 Rank,RestaurantName,Category.RestaurantType FROM Restaurant,Category Where Restaurant.RestaurantID = Category.RestaurantID');
+            res.render ("C_Cat", {
+                result,
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('Error retrieving users from database');
+        }
     }
 )
 
